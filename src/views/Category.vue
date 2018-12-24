7cfa69d8e6f343a82ca391d984b37ffdb10d673f
DavidXia <13818094497@163.com>
2018-12-24 17:06:22 +0800

    day15 p.m

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,224 +1,48 @@
 <template>
+
 	<div class="category-page">
 		<top-bar>
 			<input class="search1" type="search" v-model="keyword" placeholder="海尔京东超级品牌日    每满千减百" />
 		</top-bar>
 
-		<ul class="list1" style="transform: translateY(0px);" id="category2">
-			<li class="cur" m_index="1" m_cid="WQR2006" commonused="false">
-				<a class="J_ping" id="category6">热门推荐</a>
-			</li>
-			<li m_index="2" m_cid="WQ1980" id="category7" commonused="true">
-				<a class="J_ping">手机数码</a>
-			</li>
-			<li m_index="3" m_cid="WQ1982" id="category8" commonused="true">
-				<a class="J_ping">家用电器</a>
-			</li>
-			<li m_index="4" m_cid="WQ1981" id="category9" commonused="true">
-				<a class="J_ping">电脑办公</a>
-			</li>
-			<li m_index="5" m_cid="WQ1988" id="category10" commonused="true">
-				<a class="J_ping">计生情趣</a>
-			</li>
-			<li m_index="6" m_cid="WQ2044" id="category11" commonused="false">
-				<a class="J_ping">美妆护肤</a>
-			</li>
-			<li m_index="7" m_cid="WQ2046" id="category12" commonused="false">
-				<a class="J_ping">个护清洁</a>
-			</li>
-			<li m_index="8" m_cid="WQ1990" id="category13" commonused="true">
-				<a class="J_ping">汽车用品</a>
-			</li>
-			<li m_index="9" m_cid="WQ1968" id="category14" commonused="true">
-				<a class="J_ping">京东超市</a>
-			</li>
-			<li m_index="10" m_cid="WQ1972" id="category15" commonused="true">
-				<a class="J_ping">男装</a>
-			</li>
-			<li m_index="11" m_cid="WQ1974" id="category16" commonused="true">
-				<a class="J_ping">男鞋</a>
-			</li>
-			<li m_index="12" m_cid="WQ1973" id="category17" commonused="true">
-				<a class="J_ping">女装</a>
-			</li>
-			<li m_index="13" m_cid="WQ1975" id="category18" commonused="false">
-				<a class="J_ping">女鞋</a>
-			</li>
-			<li m_index="14" m_cid="WQ1985" id="category19" commonused="true">
-				<a class="J_ping">母婴童装</a>
-			</li>
-			<li m_index="15" m_cid="WQ1996" id="category20" commonused="false">
-				<a class="J_ping">图书音像</a>
-			</li>
-			<li m_index="16" m_cid="WQ1989" id="category21" commonused="false">
-				<a class="J_ping">运动户外</a>
-			</li>
-			<li m_index="17" m_cid="WQ1976" id="category22" commonused="true">
-				<a class="J_ping">内衣配饰</a>
-			</li>
-			<li m_index="18" m_cid="WQ1983" id="category23" commonused="false">
-				<a class="J_ping">食品生鲜</a>
-			</li>
-			<li m_index="19" m_cid="WQ1984" id="category24" commonused="false">
-				<a class="J_ping">酒水饮料</a>
-			</li>
-			<li m_index="20" m_cid="WQ1992" id="category25" commonused="false">
-				<a class="J_ping">家具家装</a>
-			</li>
-			<li m_index="21" m_cid="WQ1991" id="category26" commonused="false">
-				<a class="J_ping">家居厨具</a>
-			</li>
-			<li m_index="22" m_cid="WQ1977" id="category27" commonused="false">
-				<a class="J_ping">箱包手袋</a>
-			</li>
-			<li m_index="23" m_cid="WQ1979" id="category28" commonused="false">
-				<a class="J_ping">钟表珠宝</a>
-			</li>
-			<li m_index="24" m_cid="WQ1986" id="category29" commonused="false">
-				<a class="J_ping">玩具乐器</a>
-			</li>
-			<li m_index="25" m_cid="WQ1987" id="category30" commonused="false">
-				<a class="J_ping">医药保健</a>
-			</li>
-			<li m_index="26" m_cid="WQ1994" id="category31" commonused="false">
-				<a class="J_ping">宠物生活</a>
-			</li>
-			<li m_index="27" m_cid="WQ1993" id="category32" commonused="false">
-				<a class="J_ping">礼品鲜花</a>
-			</li>
-			<li m_index="28" m_cid="WQ1998" id="category33" commonused="false">
-				<a class="J_ping">农资绿植</a>
-			</li>
-			<li m_index="29" m_cid="WQ1995" id="category34" commonused="false">
-				<a class="J_ping">生活旅行</a>
-			</li>
-			<li m_index="30" m_cid="WQ1970" id="category35" commonused="false">
-				<a class="J_ping">奢侈品</a>
-			</li>
-			<li m_index="31" m_cid="WQ1971" id="category36" commonused="false">
-				<a class="J_ping">海囤全球</a>
-			</li>
-			<li m_index="32" m_cid="WQ1997" id="category37" commonused="false">
-				<a class="J_ping">艺术邮币</a>
-			</li>
-			<li m_index="33" m_cid="WQ2002" id="category38" commonused="false">
-				<a class="J_ping">二手商品</a>
-			</li>
-			<li m_index="34" m_cid="WQ1999" id="category39" commonused="false">
-				<a class="J_ping">特产馆</a>
-			</li>
-			<li m_index="35" m_cid="WQ2000" id="category40" commonused="false">
-				<a class="J_ping">京东金融</a>
-			</li>
-			<li m_index="36 " m_cid="WQ1969 " id="category41 " commonused="false ">
-				<a class="J_ping ">国际名牌</a>
-			</li>
-			<li m_index="37 " m_cid="WQ2001 " id="category42 " commonused="false ">
-				<a class="J_ping ">拍卖</a>
-			</li>
-			<li m_index="38 " m_cid="WQ2008 " id="category43 " commonused="false ">
-				<a class="J_ping ">房产</a>
-			</li>
-		</ul>
+		<div class="container weui-flex">
 
-		<div class="category-content">
+			<div class="aside" ref="aside" @touchstart="asideTouchStart" @touchmove="asideTouchMove" @touchend="asideTouchEnd">
 
-			<div class="img1"><img src="//img20.360buyimg.com/mcoss/jfs/t1/17970/5/1976/51492/5c189253E07d143ed/5df8a82661807e7f.png"></div>
-			<div class="fenlei">
-				<h4>热门分类</h4>
-				<ul class="jd-category-style-1">
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
+				<ul ref="aside-ul" :style="{top:asideTop+'px'}" :class="{transition:transition}">
+					<li :class="{active:navIndex==index}" v-for="(item,index) in nav" v-text="item.title" @click="navIndex=index;getContentList(item.id);"> </li>
 				</ul>
-				<ul class="jd-category-style-1">
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-				</ul>
-				<ul class="jd-category-style-1">
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-				</ul>
-				
-				<h4>家电热搜</h4>
-				<ul class="jd-category-style-1">
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-				</ul>
-				<ul class="jd-category-style-1">
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-				</ul>
-				<ul class="jd-category-style-1">
-					<li>
-						<img src="../assets/images/5be6ebd8Nb07ef492.png" />
-						<p>手机</p>
-					</li>
-				</ul>
-				
+
 			</div>
+
+			<div class="content weui-flex__item" ref="content" @touchstart="contentTouchStart" @touchmove="contentTouchMove" @touchend="contentTouchEnd">
+
+				<div class="category-list" ref="category-list" :style="{top:contentTop+'px'}">
+
+					<div class="category-image">
+						<img :src="products.image">
+					</div>
+
+					<div class="category-jd" v-for="(row,i) in products.category">
+
+						<div class="title" v-text="row.title"></div>
+						<ul class="clear">
+							<li class="prodcut left" v-for="(col,j) in row.list">
+								<img :src="col.image">
+								<div class="product-name" v-text="col.name"></div>
+							</li>
+						</ul>
+
+					</div>
+
+				</div>
+
+			</div>
+
 		</div>
 
-		<bottom style="clear: both;"></bottom>
+		<bottom></bottom>
 	</div>
-
 </template>
 
 <script>
@@ -226,19 +50,253 @@
 	import Bottom from "@/components/Footer"
 	export default {
 		data() {
-			return {};
+			return {
+				nav: [{
+					id: 1,
+					'title': '热门推荐'
+				}, {
+					id: 1,
+					'title': '手机数码'
+				}, {
+					id: 1,
+					'title': '家用电器'
+				}, {
+					id: 1,
+					'title': '电脑办公'
+				}, {
+					id: 1,
+					'title': '计生情趣'
+				}, {
+					id: 1,
+					'title': '美妆护肤'
+				}, {
+					id: 1,
+					'title': '个护清洁'
+				}, {
+					id: 1,
+					'title': '汽车用品'
+				}, {
+					id: 1,
+					'title': '京东超市'
+				}, {
+					id: 1,
+					'title': '男装'
+				}, {
+					id: 1,
+					'title': '男鞋'
+				}, {
+					id: 1,
+					'title': '女装'
+				}, {
+					id: 1,
+					'title': '女鞋'
+				}, {
+					id: 1,
+					'title': '母婴童装'
+				}, {
+					id: 1,
+					'title': '图书音像'
+				}, {
+					id: 1,
+					'title': '运动户外'
+				}],
+				navIndex: 0,
+				asideTop: 0,
+				asideTopTmp: 0,
+				asideStartY: 0,
+				minTop: 0,
+				transition: false,
+
+				category: [{
+					image: require('../assets/images/xinrenlibao.png'),
+					category: [{
+						title: '热门分类',
+						list: [{
+							image: require('../assets/images/phone.png'),
+							name: '手机'
+						}, {
+							image: require('../assets/images/phone.png'),
+							name: '手机'
+						}, {
+							image: require('../assets/images/phone.png'),
+							name: '手机'
+						}, {
+							image: require('../assets/images/phone.png'),
+							name: '手机'
+						}, {
+							image: require('../assets/images/phone.png'),
+							name: '手机'
+						}, {
+							image: require('../assets/images/phone.png'),
+							name: '手机'
+						}, {
+							image: require('../assets/images/phone.png'),
+							name: '手机'
+						}, {
+							image: require('../assets/images/phone.png'),
+							name: '手机'
+						}, {
+							image: require('../assets/images/phone.png'),
+							name: '手机'
+						}]
+					}, {
+						title: '家电搜索',
+						list: [{
+							image: require('../assets/images/reshuihu.jpg'),
+							name: '热水壶'
+						}, {
+							image: require('../assets/images/reshuihu.jpg'),
+							name: '热水壶'
+						}, {
+							image: require('../assets/images/reshuihu.jpg'),
+							name: '热水壶'
+						}]
+					}]
+				}, {
+					image: require('../assets/images/xinrenlibao.png'),
+					category: [{
+						title: '热门分类',
+						list: [{
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}, {
+							image: require('../assets/images/biyuntao.jpg'),
+							name: '计生用品'
+						}]
+					}, {
+						title: '家电搜索',
+						list: [{
+							image: require('../assets/images/huahzuangpin.jpg'),
+							name: '化妆品'
+						}, {
+							image: require('../assets/images/huahzuangpin.jpg'),
+							name: '化妆品'
+						}, {
+							image: require('../assets/images/huahzuangpin.jpg'),
+							name: '化妆品'
+						}]
+					}]
+				}],
+				products: [],
+				contentTop: 0,
+				contentTopTmp: 0,
+				contentStartY: 0,
+				contentMinTop: 0,
+			};
 		},
 		methods: {
+			asideTouchStart(event) {
+				this.transition = false;
+				this.asideStartY = event.changedTouches[0].clientY;
+			},
+			asideTouchMove(event) {
+				var y = event.changedTouches[0].clientY;
 
+				var t = this.asideTopTmp + y - this.asideStartY;
+				t = t < this.minTop ? this.minTop : t;
+				t = t > 150 ? 150 : t;
+
+				this.asideTop = t;
+			},
+			asideTouchEnd(event) {
+				this.transition = true;
+				this.asideTop = this.asideTop > 0 ? 0 : this.asideTop;
+				this.asideTop = this.asideTop < (this.minTop + 150) ? (this.minTop + 150) : this.asideTop;
+				this.asideTopTmp = this.asideTop;
+			},
+
+			contentTouchStart(event) {
+				this.contentStartY = event.changedTouches[0].clientY;
+			},
+			contentTouchMove(event) {
+				var y = event.changedTouches[0].clientY;
+
+				var t = this.contentTopTmp + y - this.contentStartY;
+				t = t < this.contentMinTop ? this.contentMinTop : t;
+				t = t > 150 ? 150 : t;
+
+				this.contentTop = t;
+			},
+			contentTouchEnd(event) {
+				this.contentTop = this.contentTop > 0 ? 0 : this.contentTop;
+				this.contentTop = this.contentTop < (this.contentMinTop + 150) ? (this.contentMinTop + 150) : this.contentTop;
+				this.contentTopTmp = this.contentTop;
+			},
+
+			getContentList(id) {
+				this.products = { ...this.category[(Math.random() > 0.5 ? 0 : 1)]
+				};
+				console.log(this.products)
+			}
+		},
+		mounted() {
+			console.log(document.querySelector(".category-page .aside").clientHeight);
+			console.log(document.querySelector(".category-page .aside ul").clientHeight);
+
+			console.log(this.$refs['aside'].clientHeight);
+			console.log(this.$refs['aside-ul'].clientHeight);
+
+			this.minTop = this.$refs['aside'].clientHeight - 150 - this.$refs['aside-ul'].clientHeight;
+
+			console.log(this.minTop);
+
+			this.contentMinTop = this.$refs['content'].clientHeight - 150 - this.$refs['category-list'].clientHeight;
+		},
+		created() {
+			this.getContentList();
+		},
+		updated() {
+			this.contentMinTop = this.$refs['content'].clientHeight - 150 - this.$refs['category-list'].clientHeight;
 		},
 		components: {
+			Bottom,
 			TopBar,
-			Bottom
 		}
 	}
 </script>
 
 <style>
+	html,
+	body,
+	#app,
+	.category-page {
+		height: 100%;
+	}
+	
+	.category-page .top-bar {
+		position: fixed;
+	}
+	
 	.search1 {
 		width: 100%;
 		height: 30px;
@@ -250,118 +308,115 @@
 		border: 0px;
 	}
 	
-	element.style {
-		transform: translateY(0px);
+	.category-page {}
+	
+	.category-page .search-bar {
+		background-color: #e43130;
 	}
 	
-	.category-categoryNewUi .jd-category-tab ul {
+	.category-page .container {
+		background-color: #FFFFFF;
+		height: 100%;
+		padding: 45px 0px 50px;
+		box-sizing: border-box;
+	}
+	
+	.category-page .aside {
+		height: 100%;
 		width: 85px;
-	}
-	
-	.category-viewport li,
-	.category-viewport ul {
-		display: block;
-		margin: 0;
-		padding: 0;
-		list-style-type: none;
-	}
-	
-	ul,
-	menu,
-	dir {
-		display: block;
-		list-style-type: disc;
-		margin-block-start: 0em;
-		margin-block-end: 0em;
-		margin-inline-start: 0px;
-		margin-inline-end: 0px;
-		padding-inline-start: 0px;
-		list-style: none;
-	}
-	
-	.jd-category-tab li {
-		height: 46px;
-		line-height: 46px;
-		background: #fff;
-		text-align: center;
-		position: relative;
-	}
-	
-	.category-viewport li,
-	.category-viewport ul {
-		display: block;
-		margin: 0;
-		padding: 0;
-		list-style-type: none;
-	}
-	
-	.J_ping {
-		display: block;
-		margin-left: 0px;
-		width: 85px;
-		height: 46px;
-		line-height: 46px;
-		text-decoration: none;
-		font-size: 14px;
-		color: #080808;
 		overflow: hidden;
-		text-overflow: ellipsis;
-		background-color: whitesmoke;
-		text-align: center;
+		position: relative;
+		background-color: #f8f8f8;
 	}
 	
-	#category6 {
-		color: #e93b3d;
+	.category-page .aside ul {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+		width: 100%;
+		position: absolute;
+		left: 0px;
+		top: 0px;
+		border-right: solid 1px #CCCCCC;
+		border-top: solid 1px #CCCCCC;
+		box-sizing: border-box;
+	}
+	
+	.transition {
+		-webkit-transition: top 0.3s linear;
+		transition: top 0.3s linear;
+	}
+	
+	.category-page .aside li {
+		height: 46px;
+		line-height: 46px;
+		text-align: center;
+		border-bottom: solid 1px #CCCCCC;
+		box-sizing: border-box;
+		color: #333;
+		font-size: 14px
+	}
+	
+	.category-page .aside li.active {
+		color: #f23030;
 		background-color: white;
 	}
 	
-	.category-page ul {
-		display: block;
-		width: 85px;
-		float: left;
-	}
-	
-	.list1 li {
-		line-height: 0px;
-	}
-	
-	.category-content {
-		float: left;
-		width: 275px;
-	}
-	
-	.category-content img {
-		width: 254px;
-		padding-top: 5px;
-		padding-left: 10px;
-	}
-	
-	.fenlei {
-		margin: 19px 7px 0;
-	}
-	
-	.jd-category-content .jd-category-style-1 {
-		border: 0;
-		font-size: 0;
-		padding: 7px 10px 0;
+	.category-page .content {
 		overflow: hidden;
+		position: relative;
 	}
 	
-	.jd-category-style-1 li {
-		float: left;
+	.category-list {
+		width: 100%;
+		position: absolute;
+		top: 0px;
+		left: 0px;
 	}
 	
-	.jd-category-style-1 img {
-		width: 60px;
-		height: 60px;
+	.category-image {
+		padding: 7px 7px 0px;
+		box-sizing: border-box;
 	}
 	
-	.jd-category-style-1 p {
+	.category-image img {
+		width: 100%;
+	}
+	
+	.category-jd {
+		padding: 19px 7px 0px;
+		box-sizing: border-box;
+	}
+	
+	.category-jd .title {
+		font-size: 14px;
+		color: #333;
+		margin: 0;
+		padding: 0;
+		line-height: 1em;
+		font-weight: 700;
+		color: #080808;
+	}
+	
+	.category-jd ul {
+		margin: 9px 0px 0px;
+		padding: 0px;
+	}
+	
+	.category-jd li {
+		width: 33.333%;
+		list-style: none;
+		text-align: center;
+	}
+	
+	.category-jd li img {
+		width: 70px;
+		height: 70px;
+	}
+	
+	.category-jd li .product-name {
 		color: #333;
 		height: 35px;
-		margin-top: 5px;
-		padding-left: 3px;
-		font-size: 12px;
-		text-align: center;
+		margin-top: 2px;
 	}
 </style>
--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,35 +1,91 @@
 <template>
-	
+
 	<div>
-		{{$route.params.id}}
-		
-		<router-link :to="'/product/'+id+'/goods'">商品</router-link>
-		<router-link :to="'/product/'+id+'/detail'">详情</router-link>
-		<router-link :to="'/product/'+id+'/comment'">评价</router-link>
-		<router-link :to="'/product/'+id+'/recommend'">推荐</router-link>
-		<router-view/>
+
+		<top-bar>
+
+			<ul>
+				<li :class="{active:navIndex==index}" v-for="(item,index) in nav" @click="navIndex=index"> <span v-text="item.title"></span> </li>
+			</ul>
+
+		</top-bar>
+
+		<div id="product" style="height: 300px;background-color: red;"></div>
+		<div id="comment" style="height: 300px;background-color: green;"></div>
+		<div id="detail" style="height: 300px;background-color: blue;"></div>
+		<div id="recommend" style="height: 300px;background-color: deeppink;" @click="scroll" ></div>
+
 	</div>
-	
+
 </template>
 
 <script>
-	
-	export default{
-		data(){
-			return{
-				id:0,
+	import TopBar from "@/components/TopBar";
+	export default {
+		data() {
+			return {
+				nav: [{
+					ref: '',
+					title: '商品'
+				}, {
+					ref: '',
+					title: '评价'
+				}, {
+					ref: '',
+					title: '详情'
+				}, {
+					ref: '',
+					title: '推荐'
+				}],
+				navIndex: 0,
 			};
-		},created(){
-			this.id = this.$route.params.id;
+		},
+		
+		methods:{
+			scroll(){
+				
+				
+				
+				document.body.scrollTop = 0;
+			}
+		},
+		mounted() {
+			
+			console.log(document.body.scrollTop);
+			
+		},
+		components: {
+			TopBar
 		}
 	}
-	
 </script>
 
 <style scoped>
 	
-	a{
-		margin: 5px;
-		font-size: 20px;
+	.top-bar{
+		width: 100%;
+		position: fixed;
+		top: 0px;
+		left: 0px;
+	}
+	.top-bar ul {
+		margin: 0;
+		padding: 0;
+		text-align: center;
+		list-style: none;
+		padding: 0px 40px;
+	}
+	
+	.top-bar ul li {
+		float: left;
+		width: 25%;
+	}
+	
+	.top-bar ul li.active span {
+		color: #e4393c;
+		font-weight: bold;
+		padding-left: 13px;
+		background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAYBAMAAAABjmA/AAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAnUExURek7PUdwTO49Peg6Peo6Puc6Pek6POk6POg6POg7PeY8POg6POk6PApBjXgAAAANdFJOU/8ADttQPYvEqJYqW/KuifCQAAAAjklEQVQI10XOsRGCQBAF0D8eA6Imn4ERMh0tgDE2UDqgA2Ij7UA6gA6MjCnB0vzLBlxw92Z379+B/F7DgwQraI3C05AQc0ElfBCac40NOlzIE2LUuJM5gtpHMtPxc+yW1tuH15j8+hYHD1yhdPQoHO3yKG+GSNgbXsI81AocgNS+qlgLFwrrGNjFdFS9tj82HhIeoIR1QQAAAABJRU5ErkJggg==) no-repeat 0;
+		background-size: 8px;
 	}
 </style>
